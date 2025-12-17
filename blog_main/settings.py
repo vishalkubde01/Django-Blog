93d80732e7f67283a9db4420ea9c97688b40a21a
vishalkubde01 <kubdevishal33@gmail.com>
2025-12-17 15:51:06 +0530

    Implemented CRUD operations to the Categories

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'assign',
     'crispy_forms',
     'crispy_bootstrap4',
+    'dashboards',
 ]
 
 MIDDLEWARE = [
--- blog_main/urls.py
+++ blog_main/urls.py
@@ -26,8 +26,14 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/', include('blog.urls')),
     path('blogs/<slug:slug>/', BlogView.blogs, name="blogs"),
+
+
     path('blogs/search/', BlogView.search, name='search'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name="logout"),
+
+    #dashboards
+
+    path('dashboard/', include('dashboards.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
